fee2a4a4b584de72f9945a11541fc689245bd601
IanaCris <ianasousati@gmail.com>
2021-06-09 22:08:21 -0300

    import file with multer

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,10 @@
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const importCategoryUseCase = new ImportCategoryUseCase();
+
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
+
+export { importCategoryController };
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { createSpecificationController } from "../modules/cars/useCases/CreateSpecification";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
