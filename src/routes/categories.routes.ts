27e49d0b0049283da76032774418dcede42424cc
IanaCris <ianasousati@gmail.com>
2021-06-16 22:46:03 -0300

    docker-compose

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
+  console.log("testandooooo");
   return createCategoryController.handle(request, response);
 });
 
