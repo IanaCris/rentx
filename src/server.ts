7d8f676ff428bf7dae7bdb6c8b00029da9be935c
IanaCris <ianasousati@gmail.com>
2021-06-21 22:52:55 -0300

    dependency injection createCategory

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import "reflect-metadata";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
+import "./database";
+
+import "./shared/container";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-import "./database";
 
 const app = express();
 
