796b953cdb0001b3df9873ba15225a0055873395
IanaCris <ianasousati@gmail.com>
2021-06-21 22:03:41 -0300

    refactorind code useCases

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = null;
 
 const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
