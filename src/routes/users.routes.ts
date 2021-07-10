959f31767b30d46d7df5d64924d3d0eb61ead624
IanaCris <ianasousati@gmail.com>
2021-07-09 23:44:36 -0300

    update user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRouters = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouters.post("/", createUserController.handle);
+
+usersRouters.post("/avatar", updateUserAvatarController.handle);
 
 export { usersRouters };
