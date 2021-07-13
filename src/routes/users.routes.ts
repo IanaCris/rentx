1cbe0f89c8e7f03bdcf38680946c2f9a90d90fcc
IanaCris <ianasousati@gmail.com>
2021-07-12 22:07:57 -0300

    update avatar user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRouters = Router();
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRouters.post("/", createUserController.handle);
 
-usersRouters.post("/avatar", updateUserAvatarController.handle);
+usersRouters.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRouters };
