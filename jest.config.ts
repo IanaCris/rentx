bb1f5d9b7eb26f1dc2861476c1bb8c4a1c6bc385
IanaCris <ianasousati@gmail.com>
2021-07-13 23:02:08 -0300

    config paths imports

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,7 @@
-/*
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { pathsToModuleNameMapper } from "ts-jest/utils";
 
- * For a detailed explanation regarding each configuration property and type check, visit:
-
- * https://jestjs.io/docs/configuration
-
- */
+import { compilerOptions } from "./tsconfig.json";
 
 export default {
   // All imported modules in your tests should be mocked automatically
@@ -121,7 +118,9 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
 
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 
