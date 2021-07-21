03396df885bc1258e30072a08b5a7332c7f6a157
IanaCris <ianasousati@gmail.com>
2021-07-21 00:06:35 -0300

    update seed user admin

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -1,3 +1,16 @@
+import { hash } from "bcryptjs";
 import { getConnection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
 
-async function create() {}
+async function create() {
+  const connection = getConnection();
+
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, admin, created_at)
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, ${new Date()})
+    `
+  );
+}
