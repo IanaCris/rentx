4b4c4d2f02fcc8ce86800ef1dfccf477fb8bfb4d
IanaCris <ianasousati@gmail.com>
2021-07-22 10:13:58 -0300

    connection seed user admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,15 +3,14 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
-
 import "@shared/container";
-
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
