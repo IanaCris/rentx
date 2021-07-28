f6ca7c982b3c86543f062ca027b64a939a325625
IanaCris <ianasousati@gmail.com>
2021-07-27 22:15:41 -0300

    migration create table rentals

--- src/shared/infra/typeorm/migrations/1625334092288-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1625334092288-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1625334092288 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
---
+++ src/shared/infra/typeorm/migrations/1627406399189-CreateRentals.ts
@@ -0,0 +1,75 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRentals1627406399189 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "rentals",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "start_date",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "end_date",
+            type: "timestamp",
+          },
+          {
+            name: "expect_return_date",
+            type: "timestamp",
+          },
+          {
+            name: "total",
+            type: "numeric",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FkCarRental",
+            referencedTableName: "cars",
+            referencedColumnNames: ["id"],
+            columnNames: ["car_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+          {
+            name: "FkUserRental",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("rentals");
+  }
+}
