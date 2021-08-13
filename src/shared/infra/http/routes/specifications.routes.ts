2e544a7b22c85f3bb912108386d7a9255d715147
IanaCris <ianasousati@gmail.com>
2021-08-13 00:01:46 -0300

    routes

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { CreateSpecificationController } from "@modules/cars/useCases/CreateSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
