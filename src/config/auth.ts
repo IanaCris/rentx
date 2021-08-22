d3e341a49bca08da43ccf222bf1d64a201a7f30a
IanaCris <ianasousati@gmail.com>
2021-08-21 22:04:17 -0300

    refresh token user

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "2ed1d29fecf62b8e5742b69b38552a6d",
+  expires_in_token: "15min",
+  secret_refresh_token: "99030a97eb55a39613cadb6e89af83f4",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -14,6 +14,8 @@ import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRep
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -40,4 +42,9 @@ container.registerSingleton<ICarsImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   "RentalsRepository",
   RentalsRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  "UsersTokensRepository",
+  UsersTokensRepository
 );
