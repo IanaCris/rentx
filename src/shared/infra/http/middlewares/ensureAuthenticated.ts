a63e5eb47be8d311ccd40f5eaf63ee01476e6a08
IanaCris <ianasousati@gmail.com>
2021-08-22 00:56:16 -0300

    refresh token controller

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -14,6 +15,7 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = request.headers.authorization;
+  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -25,11 +27,13 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      "2ed1d29fecf62b8e5742b69b38552a6d"
+      auth.secret_refresh_token
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = usersRepository.findById(user_id);
+    const user = usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists!", 401);
--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/autenthicateUser/AuthenticateUserController";
-
+mport { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
+i
 const authenticateRoutes = Router();
 
 const authenticateUseController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", authenticateUseController.handle);
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRoutes };
