e469e03feb0e4b67c5c96ada2d76dce0a9fb3aef
IanaCris <ianasousati@gmail.com>
2021-08-26 00:04:27 -0300

    template email

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
 
@@ -21,6 +22,15 @@ class SendForgotPasswordMailUseCase {
   ) {}
 
   async execute(email: string): Promise<void> {
+    const templatePath = resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPassword.hbs"
+    );
+
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
@@ -37,10 +47,16 @@ class SendForgotPasswordMailUseCase {
       expires_date,
     });
 
+    const variables = {
+      name: user.name,
+      link: `${process.env.FORGOT_MAIL_URL}${token}`,
+    };
+
     await this.mailProvider.sendMail(
       email,
       "Recuperação de senha",
-      `O link para o reset é ${token}`
+      variables,
+      templatePath
     );
   }
 }
